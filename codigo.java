cbb0abfd80f31c50cf45808207e1c17f9d10da3d
Gallegomacias <gallegomaciasnicolas@gmail.com>
2024-10-17 08:22:34 -0500

    Update codigo.java

--- codigo.java
+++ codigo.java
@@ -1,20 +1,52 @@
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
-        int numeroFinal = 0;
-        int sumaAcumulada = 0;
-        int contador  = 0;
-
-        Scanner lector = new Scanner(System.in);
-        System.out.print("Ingrese el número que desea calcular: ");
-        numeroFinal = lector.nextInt();
-
-        while (numeroFinal >= contador) {
-            sumaAcumulada += contador;
-            icontador++;
+        double edad = 0, estadoCivil = 0, estatura = 0, sexo = 0;
+        double promedioEdad = 0, promedioEstatura = 0, porcentajePersonas = 0;
+        int contadorPersonas = 0, cantidadPersonas = 2;
+        double acumuladorEdad = 0, acumuladorEstatura = 0;
+        
+        
+        Scanner input = new Scanner(System.in);
+        
+        
+        for (int numeroPersona = 1; numeroPersona <= cantidadPersonas; numeroPersona++) {
+            System.out.println("Ingrese la edad de la persona " + numeroPersona + ": ");
+            edad = input.nextDouble();
+            
+            System.out.println("Ingrese el estado civil de la persona " + numeroPersona + " (1. Soltero, 2. Casado): ");
+            estadoCivil = input.nextDouble();
+            
+            System.out.println("Ingrese la altura de la persona " + numeroPersona + " en cm: ");
+            estatura = input.nextDouble();
+            
+            System.out.println("Ingrese el sexo de la persona " + numeroPersona + " (1. Hombre, 2. Mujer): ");
+            sexo = input.nextDouble();
+            
+           
+            if (edad > 18 && estadoCivil == 1 && estatura > 170 && sexo == 1) {
+                contadorPersonas++;
+                acumuladorEdad += edad;
+                acumuladorEstatura += estatura;
+            }
         }
-
-        System.out.println("La suma de los primeros " + numeroFinal + " números naturales es igual a " + sumaAcumulada);
+        
+        
+        if (contadorPersonas > 0) {
+            promedioEdad = acumuladorEdad / contadorPersonas;
+            promedioEstatura = acumuladorEstatura / contadorPersonas;
+            porcentajePersonas = (double) contadorPersonas / cantidadPersonas * 100;
+            
+            
+            System.out.println("El promedio de edad de las personas que cumplen es: " + promedioEdad + " años");
+            System.out.println("El promedio de estatura de las personas que cumplen es: " + promedioEstatura + " cm");
+            System.out.println("El porcentaje de las personas que cumplen con la condición es del: " + porcentajePersonas + " %");
+        } else {
+            System.out.println("Ninguna persona cumple con las condiciones.");
+        }
+        
+        
+        input.close();
     }
 }
