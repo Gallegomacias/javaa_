38f64f97fb39ea1020ee2c144e4299b3bdd0ecff
Laboratorio 307 <estudiante307@unicamacho.co>
2024-10-17 07:53:22 -0500

    se sube repositorio

---
+++ codigo.java
@@ -0,0 +1,20 @@
+mport java.util.Scanner;
+
+public class SumaNaturales {
+    public static void main(String[] args) {
+        int numeroFinal = 0;
+        int sumaAcumulada = 0;
+        int contador  = 0;
+
+        Scanner lector = new Scanner(System.in);
+        System.out.print("Ingrese el número que desea calcular: ");
+        numeroFinal = lector.nextInt();
+
+        while (numeroFinal >= contador) {
+            sumaAcumulada += contador;
+            icontador++;
+        }
+
+        System.out.println("La suma de los primeros " + numeroFinal + " números naturales es igual a " + sumaAcumulada);
+    }
+}
